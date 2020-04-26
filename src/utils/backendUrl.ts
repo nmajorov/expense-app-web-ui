8b9c63a9a7c51c5fa59e019a3f276ac909146923
Nikolaj Majorov <nikolaj@majorov.biz>
2020-04-26 11:38:54 +0200

    fix nginx deployment openshift

--- src/utils/backendUrl.ts
+++ src/utils/backendUrl.ts
@@ -1,3 +1,3 @@
-//export  const backEndUrl = "http://camel-rest-sql:8080/services/app/expenses";
-export  const backEndUrl = "http://localhost/api";
+//export  const backEndUrl = "/api";
+export  const backEndUrl = "/api";
 
