3a6aa00344b08da770d09b30b0449e294fe69139
Nikolaj Majorov <nikolaj@majorov.biz>
2020-12-04 21:43:56 +0100

    change deployment task

--- src/utils/backendUrl.ts
+++ src/utils/backendUrl.ts
@@ -1,3 +1,4 @@
-console.log("process.env.REACT_APP_BACKEND_URL:" + process.env.REACT_APP_BACKEND_URL)
+
 export  const backEndUrl = (process.env.REACT_APP_BACKEND_URL) ? process.env.REACT_APP_BACKEND_URL.toString() : "/api";
 
+console.debug("process.env.REACT_APP_BACKEND_URL:" + process.env.REACT_APP_BACKEND_URL)
