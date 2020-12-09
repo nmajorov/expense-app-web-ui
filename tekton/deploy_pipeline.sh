7b33278a162ed433ea09470b8853f5b9cdbdf224
Nikolaj Majorov <nikolaj@majorov.biz>
2020-12-09 18:50:49 +0100

    add build ui image tekton task

--- tekton/deploy_pipeline.sh
+++ tekton/deploy_pipeline.sh
@@ -3,11 +3,11 @@
 echo "delete existing ui pipeline first "
 CUR_BRANCH="$(git rev-parse --abbrev-ref HEAD)"
 oc delete pipelineresources.tekton.dev  expenses-web-ui-git ui-image --ignore-not-found=true
-oc delete pipelineresources.tekton.dev  expenses-web-ui-$CUR_BRANCH-git ui-image --ignore-not-found=true
-
-oc delete tasks deploy-nodejs-app test-nodejs-app --ignore-not-found=true
-
+oc delete tasks.tekton.dev build-nodejs-app  --ignore-not-found=true
+oc delete tasks.tekton.dev test-nodejs-app --ignore-not-found=true
+oc delete tasks.tekton.dev webapp-build-runtime --ignore-not-found=true
 oc delete pipelines.tekton.dev build-expenses-web-ui --ignore-not-found=true
+
 
 echo "deploy resources"
 oc create -f resources/
