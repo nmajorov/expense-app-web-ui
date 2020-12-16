fafd69334681af8d57880dee1c913a9801918676
Nikolaj Majorov <nikolaj@majorov.biz>
2020-12-16 09:45:27 +0100

    changes to master piplinerun

--- test_oidc_endpoint.sh
+++ test_oidc_endpoint.sh
@@ -4,17 +4,19 @@ echo "emulate react app call to keycloak"
 
 USER_NAME=niko
 PASSWORD=niko
+SSO_URL="http://sso-test-nm-demo.apps.ocp-cluster-1.rhlab.ch/auth/realms/basic/protocol/openid-connect/token"
+BACKEND_URL="http://expenses-backend-quarkus-nm-demo.apps.ocp-cluster-1.rhlab.ch"
 
 export access_token=$(\
 
-    curl -v  -X POST "http://localhost:7080/auth/realms/basic/protocol/openid-connect/token" \
+    curl -v  -X POST "$SSO_URL" \
     -H 'content-type: application/x-www-form-urlencoded' \
     -d "grant_type=password&username=$USER_NAME&password=$PASSWORD&client_id=app-react" | jq --raw-output '.access_token' \
 )
 
 echo "access_token: $access_token"
 
-curl -v -X GET http://localhost:3000/api/reports -H "Authorization: Bearer "$access_token
+curl -v -X GET $BACKEND_URL/reports -H "Authorization: Bearer "$access_token
 
 exit 0
 
