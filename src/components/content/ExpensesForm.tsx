cb96a82cfee2ae0e11b36116f88f807cfe5311fa
Nikolaj Majorov <nikolaj@majorov.biz>
2021-06-02 11:53:32 +0200

    fix add expense

--- src/components/content/ExpensesForm.tsx
+++ src/components/content/ExpensesForm.tsx
@@ -12,35 +12,38 @@ import { AppState } from "../../store/Store";
 // import { Report } from "../../types/Report";
 // import { SSO } from "../../types/SSO";
 // import ReportThunkActions from "../../actions/ReportThunkActions";
-import { useHistory } from "react-router-dom";
+import { RouteComponentProps, useHistory } from "react-router-dom";
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import ExpensesThunkActions from "../../actions/ExpensesThunkActions";
+
+
+type ReportParams = { reportId: string };
 
 /**
  *
  * main content wrapper
  */
-export const ExpensesForm = () => {
+export const ExpensesForm =  (routerProps: RouteComponentProps<ReportParams>) => {
 
   const history = useHistory();
   const dispatch = useDispatch();
 
-  const { sso, reports } = useSelector(
+  const { sso } = useSelector(
     (state: AppState) => {
       return {
         sso: state.ssoState.sso,
-        reports: state.reportsState.reports,
-      };
+     };
     }
   );
 
-  const { reportChanges } = useSelector(
-    (state: AppState) => {
-      return {
-        reportChanges: state.reportsState.changes,
-      };
-    }
-  );
+  // const { reportChanges } = useSelector(
+  //   (state: AppState) => {
+  //     return {
+  //       reportChanges: state.reportsState.changes,
+  //     };
+  //   }
+  // );
 
 
   /**
@@ -93,7 +96,27 @@ export const ExpensesForm = () => {
   };
 
 
+ /**
+   * handel form submit
+   * @param event  a submitted form event
+   */
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (sso.authenticated) {
+      if (isEdit) {
+      //  reports[0].name = name;
 
+       // dispatch(ReportThunkActions.updateReport(sso, reports[0]));
+      } else {
+        const reportId= routerProps.match.params.reportId.trim();
+
+        const expense:Expense = {id:NaN,amount:convertStrToAmount(amount),description,createdAT:createdAT};
+        console.info(`add expnense ${JSON.stringify(expense)} to report ${reportId}`)
+        dispatch(ExpensesThunkActions.addNewExpense(sso, reportId,expense));
+      }
+      history.push("/");
+    }
+  }
 
 
 
@@ -104,10 +127,26 @@ export const ExpensesForm = () => {
 
   const [amount, setAmount] = useState("");
   const [isAmountValid, setIsAmountValid] = useState(false);
-  const [createdAT, setCreatedAT] = useState("");
+  const [createdAT, setCreatedAT] = useState(formatDateStr(new Date()));
   const [isDateValid, setIsDateValid] = useState(true);
+  const [isEdit, setIsEdit] = useState(false);
 
 
+   /**
+   * hook to render
+   */
+    useEffect(() => {
+      if (sso.authenticated) {
+        
+        if (!(history.location.pathname.match("expenses-add"))) {
+          // we adding the report
+          setIsEdit(true);
+         // dispatch(
+           // ReportThunkActions.fetchOneReport(sso, routerProps.match.params.id)
+          // );
+        }
+      }
+    }, [dispatch]);
 
 
   return (
@@ -120,13 +159,11 @@ export const ExpensesForm = () => {
           <div className="card shadow mb-3">
             <div className="card-header py-3">
               <h6 className="m-0 font-weight-bold text-primary">
-                {/**  this.props.editExpenseId ? "Edit": "Add" *
-                   */}
-                  Expense
+                {isEdit? "Edit": "Add"} Expense
                 </h6>
             </div>
             <div className="card-body">
-              <Form>
+              <Form onSubmit={handleSubmit}>
                 <div className="col-sm-10">
                   <Form.Group>
 
