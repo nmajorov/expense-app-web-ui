19c7f6114c0c2518b00d5509c560934347bd4075
Nikolaj Majorov <nikolaj@majorov.biz>
2021-07-06 17:52:27 +0200

    add change deployment

--- tekton/deploy_pipeline.sh
+++ tekton/deploy_pipeline.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+PROJECT=$(oc  project -q)                                                                                                                                         
+                                                                                                                                                                  
+DIRNAME=`dirname "$0"`
+
 echo "delete existing tasks first "
 #CUR_BRANCH="$(git rev-parse --abbrev-ref HEAD)"
 oc delete pipelineresources.tekton.dev  expenses-web-ui-git ui-image --ignore-not-found=true
@@ -14,7 +18,11 @@ echo
 echo
 
 echo "deploy resources"
-oc create -f resources/
+cat "$DIRNAME/resources/image-resource.yaml" | sed -e "s/nm-demo/$PROJECT/g" | oc create -f -                                                                     
+
+
+oc apply -f "$DIRNAME/resources/git-resource.yaml"
+oc apply -f "$DIRNAME/resources/pvc-workspace.yaml"
 
 echo "deploy tasks"
 oc create -f tasks/
