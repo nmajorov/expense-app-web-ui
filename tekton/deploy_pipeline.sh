fe9bd99fdffe1347f3245d2d66cf4eecf8e260cd
Nikolaj Majorov <nikolaj@majorov.biz>
2022-03-31 01:50:39 +0200

    add clean up tasks

--- tekton/deploy_pipeline.sh
+++ tekton/deploy_pipeline.sh
@@ -11,6 +11,7 @@ oc delete tasks.tekton.dev build-nodejs-app  --ignore-not-found=true
 oc delete tasks.tekton.dev test-nodejs-app --ignore-not-found=true
 oc delete tasks.tekton.dev webapp-build-runtime --ignore-not-found=true
 oc delete tasks.tekton.dev deploy-nodejs-app --ignore-not-found=true
+oc delete tasks.tekton.dev clean-up deploy-nodejs-app --ignore-not-found=true
 echo "delete pipeline"
 oc delete pipelines.tekton.dev build-expenses-web-ui --ignore-not-found=true
 
